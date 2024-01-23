bb79c0074bee6511ced10fa01a89b67e2f3343e6
VATRY Etienne <etienne.vatry@telecomnancy.eu>
2024-01-23 18:17:33 +0100

    début de la gestion des noeuds à l'aide d'une table de hachage

--- src/Morpion.c
+++ src/Morpion.c
@@ -151,7 +151,7 @@ void jouerOrdi(Grille_t* grille){
     racine->max = true;
 
     // on construit l'arbre
-    construireArbre(racine, false);
+    construireArbre(racine, false, 0);
 
     // on met ensuite à jour les valeurs des noeuds
     evaluerArbre(racine);
@@ -171,7 +171,7 @@ void jouerOrdi(Grille_t* grille){
     detruireArbre(racine);
 }
 
-void construireArbre(noeud_t* racine, bool joueur){
+void construireArbre(noeud_t* racine, bool joueur, int profondeur){
     int victoire = victoire_aux(racine->etatGrille);
     if (victoire == 0){
         for (int i = 0 ; i < TAILLE*TAILLE ; i++){
@@ -182,14 +182,14 @@ void construireArbre(noeud_t* racine, bool joueur){
                 for (int j = 0 ; j < TAILLE*TAILLE ; j++) fils->etatGrille[j] = racine->etatGrille[j];
                 fils->etatGrille[i] = !joueur + 1;
                 fils->nbFils = 0;
-                fils->fils = malloc(TAILLE*TAILLE*sizeof(noeud_t));
+                fils->fils = malloc((TAILLE*TAILLE - profondeur)*sizeof(noeud_t));
 
                 // on ajoute le fils à la racine
                 racine->nbFils++;
                 racine->fils[racine->nbFils - 1] = fils;
 
                 // on vérifie si le fils est une feuille
-                if (victoire_aux(fils->etatGrille) == 0) construireArbre(fils, !joueur);
+                if (victoire_aux(fils->etatGrille) == 0) construireArbre(fils, !joueur, profondeur + 1);
                 else if (victoire_aux(fils->etatGrille) == 1) fils->valeur = -1;
                 else if (victoire_aux(fils->etatGrille) == 2) fils->valeur = 1;
                 else fils->valeur = 0;
@@ -248,4 +248,105 @@ void afficherArbre(noeud_t* racine, int profondeur){
     printf("Profondeur : %d - Valeur : %d -", profondeur, racine->valeur);
     for (int i = 0 ; i < TAILLE*TAILLE ; i++) printf(" %d ", racine->etatGrille[i]);
     printf("\n");
+}
+
+char* creerCle(int grille[TAILLE*TAILLE]){
+    char* str = malloc((TAILLE*TAILLE + 1)*sizeof(char));
+    for (int i = 0 ; i < TAILLE*TAILLE ; i++){
+        str[i] = '0' + grille[i];
+    }
+    str[TAILLE*TAILLE] = '\0';
+    return str;
+}
+
+unsigned int hash(char* cle, int tailleTable) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *cle++)) {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return hash % tailleTable;
+}
+
+table_t* initTable(int taille) {
+    table_t* table = malloc(sizeof(table_t));
+    table->taille = taille;
+    table->nbElements = 0;
+    table->elements = malloc(taille * sizeof(hache_t*));
+
+    for (int i = 0; i < taille; i++) {
+        table->elements[i] = NULL;
+    }
+
+    return table;
+}
+
+void ajouterElement(table_t* table, char* cle, noeud_t* noeud) {
+    if ( (float) table->nbElements / table->taille > 0.7 ) {
+        int index = hash(cle, table->taille);
+        hache_t* nouvelElement = malloc(sizeof(hache_t));
+        nouvelElement->cle = strdup(cle); // Duplique la clé
+        free(cle);
+        nouvelElement->noeud = noeud;
+
+        // Gestion des collisions
+        nouvelElement->suivant = table->elements[index];
+        table->elements[index] = nouvelElement;
+        table->nbElements++;
+    } else {
+        redimensionnerTable(table, table->taille * 2);
+        ajouterElement(table, cle, noeud);
+    }
+}
+
+void redimensionnerTable(table_t* table, int nouvelleTaille) {
+    hache_t** elements = table->elements;
+    int taille = table->taille;
+
+    table->elements = malloc(nouvelleTaille * sizeof(hache_t*));
+    table->taille = nouvelleTaille;
+    table->nbElements = 0;
+
+    for (int i = 0; i < nouvelleTaille; i++) {
+        table->elements[i] = NULL;
+    }
+
+    for (int i = 0; i < taille; i++) {
+        hache_t* element = elements[i];
+        while (element != NULL) {
+            ajouterElement(table, element->cle, element->noeud);
+            hache_t* suivant = element->suivant;
+            free(element);
+            element = suivant;
+        }
+    }
+
+    free(elements);
+}
+
+bool existe(table_t* table, char* cle) {
+    int index = hash(cle, table->taille);
+    hache_t* element = table->elements[index];
+
+    while (element != NULL) {
+        if (strcmp(element->cle, cle) == 0) {
+            return true; // Clé trouvée
+        }
+        element = element->suivant;
+    }
+    return false; // Clé non trouvée
+}
+
+void supprimerTable(table_t* table) {
+    for (int i = 0; i < table->taille; i++) {
+        hache_t* element = table->elements[i];
+        while (element != NULL) {
+            hache_t* suivant = element->suivant;
+            free(element->cle);
+            free(element);
+            element = suivant;
+        }
+    }
+    free(table->elements);
+    free(table);
 }
--- src/Morpion.h
+++ src/Morpion.h
@@ -1,7 +1,7 @@
 #ifndef __MORPION_H__
 #define __MORPION_H__
 
-#define TAILLE 4
+#define TAILLE 3
 
 typedef struct {
     int grille[TAILLE*TAILLE];
@@ -21,15 +21,36 @@ struct noeud_t{
     noeud_t** fils;
 };
 
+typedef struct hache_t hache_t;
+
+struct hache_t{
+    noeud_t* noeud;
+    char* cle;
+    hache_t* suivant;
+};
+
+typedef struct table_t{
+    int taille;
+    int nbElements;
+    hache_t** elements;
+} table_t;
+
 extern Grille_t* initGrille();
 extern void affichageGrille(Grille_t* grille);
 extern bool jouer(Grille_t* grille, int i, int j, bool joueur);
 extern int victoire(Grille_t* grille);
 extern void jouerOrdi(Grille_t* grille);
-extern void construireArbre(noeud_t* noeud, bool joueur);
+extern void construireArbre(noeud_t* noeud, bool joueur, int profondeur);
 extern void detruireArbre(noeud_t* noeud);
 extern bool estFeuille(noeud_t* noeud);
 extern void evaluerArbre(noeud_t* racine);
 extern void afficherArbre(noeud_t* racine, int profondeur);
+extern char* creerCle(int grille[TAILLE*TAILLE]);
+extern unsigned int hash(char* cle, int tailleTable);
+extern table_t* initTable(int taille);
+extern void ajouterElement(table_t* table, char* cle, noeud_t* noeud);
+extern void redimensionnerTable(table_t* table, int nouvelleTaille);
+extern bool existe(table_t* table, char* cle);
+extern void supprimerTable(table_t* table);
 
 #endif
